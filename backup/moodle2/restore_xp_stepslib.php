3e5631d02859eba7aced8c95b4cd74a72d99f934
Peter Dias <peter@moodle.com>
2022-04-05 19:19:50 +0800

    Backup/restore drops

--- backup/moodle2/restore_xp_stepslib.php
+++ backup/moodle2/restore_xp_stepslib.php
@@ -62,6 +62,7 @@ class restore_xp_block_structure_step extends restore_structure_step {
         $paths[] = new restore_path_element('block', '/block');
         $paths[] = new restore_path_element('config', '/block/config');
         $paths[] = new restore_path_element('filter', '/block/filters/filter');
+        $paths[] = new restore_path_element('drop', '/block/drops/drop');
 
         if ($userinfo) {
             $paths[] = new restore_path_element('xp', '/block/xps/xp');
@@ -127,6 +128,23 @@ class restore_xp_block_structure_step extends restore_structure_step {
             $this->log("block_xp: XP of user with id '{$data['userid']}' not restored, existing entry found", backup::LOG_DEBUG);
             return;
         }
+        $DB->insert_record('block_xp', $data);
+    }
+
+    /**
+     * Process the drop restore.
+     *
+     * @param $data
+     */
+    protected function process_drop($data) {
+        global $DB;
+        $data['courseid'] = $this->get_courseid();
+        $record = $DB->record_exists('block_xp_drops', ['secret' => $data['secret']]);
+        if ($record) {
+            $this->log("block_xp: Drop '{$data['name']}' not restored, existing entry found", backup::LOG_DEBUG);
+            return;
+        }
+
         $DB->insert_record('block_xp', $data);
     }
 
